944a3c1cbea49e56c48ded1105675de5da7a3764
Marrife Cloma <marrifecloma@gmail.com>
2017-10-30 22:18:59 +0000

    Changed config file to auto-detect changes to not restart server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.reload_classes_only_on_change = false
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -50,5 +52,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
