a10200cca76585c087f1f4f5c0970b7b0d033a0a
Marrife Cloma <marrifecloma@gmail.com>
2017-11-01 18:15:59 +0000

    Fixed up show all products page, added partials for the layout of the webpage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
-  def index
+  def show_all
   #  @products = Product.paginate(:page[:page], :per_page => 9).order(:category_id).order(:name)
-    @products = Product.paginate(:page => params[:page], :per_page => 9).order(:category_id).order(:name)
+    @products = Product.where('stock_quantity > 0').paginate(:page => params[:page], :per_page => 9).order(:category_id).order(:name)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  get 'products/show_all'
 
   get 'products/show'
 
