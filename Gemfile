8f7fa34cad2153346a4c6f0af7952039d5b34282
Marrife Cloma <marrifecloma@gmail.com>
2017-11-01 17:07:26 +0000

    Added slim-rails gem, converted all erb files to slim. Created products index page

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'activeadmin', github: 'activeadmin'
 gem 'carrierwave', '~> 1.0'
+gem 'will_paginate', '~> 3.1.0'
+gem 'slim-rails'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   def index
+  #  @products = Product.paginate(:page[:page], :per_page => 9).order(:category_id).order(:name)
+    @products = Product.paginate(:page => params[:page], :per_page => 9).order(:category_id).order(:name)
   end
 
   def show
