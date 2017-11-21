e20e9000fa7da9be7e1ba92ab6d5f965cd94a3b6
Marrife Cloma <marrifecloma@gmail.com>
2017-11-21 03:40:30 +0000

    Added province table, and reference to customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121032951) do
+ActiveRecord::Schema.define(version: 20171121033723) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20171121032951) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "province_id"
+    t.index ["province_id"], name: "index_customers_on_province_id"
   end
 
   create_table "orders", force: :cascade do |t|
